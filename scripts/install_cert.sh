bf845d638fe9892c9ed2451ff0f3555472202283
westhide <westhide.dev@gmail.com>
2025-05-27 08:38:08 +0800

    wip: use test1.im.avato.cc domain

--- scripts/install_cert.sh
+++ scripts/install_cert.sh
@@ -9,7 +9,7 @@ function acme_cert() {
     git clone https://github.com/acmesh-official/acme.sh.git
     cd ./acme.sh
     ./acme.sh --install -m ${domain_mail}
-    . "~/.acme.sh/acme.sh.env"
+    . ~/.acme.sh/acme.sh.env
     acme.sh --issue -d ${domain_name} --standalone
     mkdir ${domain_name}
     acme.sh --install-cert -d ${domain_name} --key-file ./${domain_name}/key.pem --fullchain-file ./${domain_name}/crt.pem --reloadcmd "service nginx force-reload"
